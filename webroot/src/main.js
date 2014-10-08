da398d206842f54a05c2f6edb93e1c340a61aafd
Jeremy Carter <Jeremy@JeremyCarter.ca>
2014-10-08 13:10:48 -0400

    Added username and timestamp.

--- webroot/src/main.js
+++ webroot/src/main.js
@@ -19,6 +19,13 @@ $(function() {  // main()
         var msg = evt.data;
         console.log("WebSocket msg received: " + msg);
         msg = $("<div/>").html(msg).text(); // Strip out html tags.
+        
+        var match = msg.search(":");
+        if (match === -1) {
+            var date = new Date();
+            msg = date.toLocaleDateString() + " " + date.toLocaleTimeString() + " | " + "Broadcast Msg: " + msg;
+        }
+        
         $("#msg_box").append('<span class="msg">' + msg + '</span>'); // Add msg to msg box.
     };
     
@@ -27,9 +34,16 @@ $(function() {  // main()
     };
     
     $("#msg_box_form").submit(function(evt) {   // On send new message.
+        var date = new Date();
+        var usr = $(this).find('[name=username]').val();
         var msg = $(this).find('[name=msg]').val();
+        
+        usr = $("<div/>").html(usr).text();
         msg = $("<div/>").html(msg).text(); // Strip out html tags.
-        if (!msg) return false; // Return if msg box is empty.
+        
+        if (!msg || !usr) return false; // Return if username or msg box is empty.
+        
+        msg = date.toLocaleDateString() + " " + date.toLocaleTimeString() + " | " + usr + ": " + msg;
         ws.send(msg);   // Send the message over websocket.
         console.log("WebSocket msg sent: "+ msg);
         
